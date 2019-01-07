d7bc4845dcebfc759ec7e681ecb2f4f9de39a368
zachatduckpin <zach@duckpindesign.com>
2019-01-07 07:43:41 -0500

    static offset edits

--- inc/meta/modules/static_image_offset.php
+++ inc/meta/modules/static_image_offset.php
@@ -25,5 +25,54 @@ $box->add_field( array(
  		'required' => 'required',
  	),
 ));
+$box->add_field( array(
+	'name' => 'Left Side CTA Text',
+	'id' => $prefix.'left_side_cta',
+	'type' => 'text',
+	'attributes'  => array(
+ 		'required'    => 'required',
+ 	),
+));
+$box->add_field( array(
+	'name' => 'Left Side CTA URL',
+	'id' => $prefix.'left_side_url',
+	'type' => 'text',
+	'attributes'  => array(
+ 		'required'    => 'required',
+ 	),
+));
+$box->add_field( array(
+	'name' => 'Right Side Image',
+	'id'   => $prefix.'right_side_image',
+	'type' => 'file',
+	'options' => array('url' => false),
+	'query_args' => array(
+		'type' => array('image/gif','image/jpeg','image/png')
+	)
+));
+$box->add_field( array(
+	'name' => 'Right Site Content',
+	'id' => $prefix.'right_side_content',
+	'type' => 'wysiwyg',
+	'attributes' => array(
+ 		'required' => 'required',
+ 	),
+));
+$box->add_field( array(
+	'name' => 'Right Side CTA Text',
+	'id' => $prefix.'right_side_cta',
+	'type' => 'text',
+	'attributes'  => array(
+ 		'required'    => 'required',
+ 	),
+));
+$box->add_field( array(
+	'name' => 'Right Side CTA URL',
+	'id' => $prefix.'right_side_url',
+	'type' => 'text',
+	'attributes'  => array(
+ 		'required'    => 'required',
+ 	),
+));
 
 ?>
--- partials/static_image_offset.php
+++ partials/static_image_offset.php
@@ -12,9 +12,27 @@
   $raw_headline = get_post_meta(get_the_id(),$instance.'_headline',true);
 	$headline = str_replace(array('{','}'), array('<span>','</span>'),$raw_headline);
 	
-	$content = get_post_meta(get_the_id(),$instance.'_left_side_content',true);
+	$left_side_img = get_post_meta(get_the_id(),$instance.'_left_side_image',true);
+	$left_side_content = get_post_meta(get_the_id(),$instance.'_left_side_content',true);
+	$left_side_cta = get_post_meta(get_the_id(),$instance.'_left_side_cta',true);
+	$left_side_url = get_post_meta(get_the_id(),$instance.'_left_side_url',true);
 	?>
 
+ <div class="offset_left">
+	 <h2 class="<?php echo $headline_style; ?>"><?php echo $headline; ?></h2>
+	 <div class="offset_left_img" style="background-image: url(<?php echo $left_side_img; ?>);"></div>
+	 <?php echo wpautop($left_side_content); ?>
+	 <p><a href="<?php echo $left_side_url; ?>" class="btn solid"><?php echo $left_side_cta; ?></a></p>
+ </div>
  
-
+ <div class="offset_right">
+	 <?php
+		$right_side_img = get_post_meta(get_the_id(),$instance.'_right_side_image',true);
+		$right_side_cta = get_post_meta(get_the_id(),$instance.'_right_side_cta',true);
+		$right_side_url = get_post_meta(get_the_id(),$instance.'_right_side_url',true);
+	?>
+	 <div class="offset_right_img" style="background-image: url(<?php echo $right_side_img; ?>);"></div>
+	 <p><a href="<?php echo $right_side_url; ?>" class="btn solid"><?php echo $right_side_cta; ?></a></p>
+ </div>
+ 
 </div>
