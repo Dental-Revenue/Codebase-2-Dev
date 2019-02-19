652e8becbc40ba8babb977bb56e9efc53cae99e4
zachatduckpin <zach@duckpindesign.com>
2019-02-19 00:16:58 -0500

    reviews testimonials edits

--- partials/reviews_testimonials_grid.php
+++ partials/reviews_testimonials_grid.php
@@ -1,4 +1,18 @@
 <?php $instance = $template_args['instance']; 
+	
+	$color = (get_post_meta(get_the_id(),$instance.'_bg_color',true)!='') ? get_post_meta(get_the_id(),$instance.'_bg_color',true) : '#ffffff' ;
+	$hex = str_replace('#', '', $color);
+	if(strlen($hex) == 3) {
+	$r = hexdec(substr($hex,0,1).substr($hex,0,1));
+	$g = hexdec(substr($hex,1,1).substr($hex,1,1));
+	$b = hexdec(substr($hex,2,1).substr($hex,2,1));
+	} else {
+	$r = hexdec(substr($hex,0,2));
+	$g = hexdec(substr($hex,2,2));
+	$b = hexdec(substr($hex,4,2));
+	}
+	$rgb = array($r, $g, $b);
+	
 	$items = get_post_meta(get_the_id(),$instance.'_items',true);
 	$appearance_info = get_option( 'appearance_info');
 	$headline_style = $appearance_info['headline_style'];
@@ -24,12 +38,12 @@
     ?>
       
       <div class="cvt-block" style="background-image: url(<?php echo $image[0]; ?>);">
-				<div class="cvt-content">
+				<div class="cvt-content" style="background-color: rgba(<?php echo $r; ?>,<?php echo $g; ?>,<?php echo $b; ?>,.5);">
 					<h3><?php echo $block['name']; ?></h3>
 					<p><?php echo $block['excerpt']; ?></p>
 					<a href="<?php echo $block['url']; ?>" class="popup-youtube">Watch Video</a>
 				</div>
-				<span><?php echo $block['name']; ?></span>
+				<span style="background-color: rgba(<?php echo $r; ?>,<?php echo $g; ?>,<?php echo $b; ?>,.5);"><?php echo $block['name']; ?></span>
 			</div>
       	
     <?php } ?>
