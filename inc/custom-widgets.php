b04cb1178dda6ad678b88a25701425050d4be9fb
zachatduckpin <zach@duckpindesign.com>
2019-02-25 12:53:26 -0500

    move/edit smile gallery widget

--- inc/custom-widgets.php
+++ inc/custom-widgets.php
@@ -19,6 +19,8 @@ if (function_exists('register_sidebar')){
 function wpb_load_widget() {
   register_widget( 'widget_appt' );
   register_widget( 'widget_blog' );
+  
+  register_widget( 'widget_gallery' );
 }
 add_action( 'widgets_init', 'wpb_load_widget' );
 
@@ -93,6 +95,105 @@ class widget_blog extends WP_Widget {
   }
 
 }
+
+
+// Gallery Widget
+
+if(!class_exists('widget_gallery')){
+class widget_gallery extends WP_Widget {
+  
+  function __construct() {parent::__construct('gallery', __('Smile Gallery', 'widget_form_domain'));}
+  
+  public function widget( $args, $instance ) {
+    
+    $queried_object = get_queried_object();
+    if($queried_object){
+      $post_id = $queried_object->ID;
+      $hide_widget = get_post_meta($post_id, 'widget_gallery_show', true);
+      $widget_cat = get_post_meta($post_id, 'widget_gallery_cat', true);
+    }
+    
+    if(!$hide_widget){
+    
+      $title = $instance['title'];
+      echo $args['before_widget'];
+      if (!empty($title)){echo $args['before_title'] . $title . $args['after_title'];}      
+        
+      if($post_id){
+        
+        echo "<div class='slick-widget-gallery'>";
+        
+        $gallery_args = array( 'post_type' => 'smile_gallery', 'orderby' => 'post_date','order' => 'DESC','posts_per_page' => 8 );
+        if($widget_cat){
+          $gallery_args = array( 'post_type' => 'smile_gallery', 'orderby' => 'post_date','order' => 'DESC','posts_per_page' => 8, 'meta_query' => array(array('key' => 'meta_patient_categories','value' => $widget_cat,'compare' => 'LIKE')));
+        }
+        $loop = new WP_Query( $gallery_args );
+        while ( $loop->have_posts() ) : $loop->the_post();
+        
+        	echo "<a href='#' class='slick-widget-gallery-img'>";
+        
+        	if(!get_post_meta(get_the_ID(), 'meta_patient_primary_id', true)){
+	        	echo wp_get_attachment_image( get_post_thumbnail_id(get_the_ID()), 'md-square' );
+        	}else{
+	        	$primary_id = get_post_meta(get_the_ID(), 'meta_patient_primary_id', true);
+						$stitch_id = get_post_meta(get_the_ID(), 'meta_patient_stitch_id', true);
+	        	if($primary_id && $stitch_id){
+            	echo wp_get_attachment_image( $stitch_id, 'md-square' );
+          	}else{
+            	echo wp_get_attachment_image( $primary_id, 'md-square' );
+          	}
+        	}
+					
+					echo "</a>";
+          
+        endwhile;
+        
+        echo "</div>";
+        
+      }
+      
+      $page = get_pages(array('meta_key' => '_wp_page_template','meta_value' => 'page-templates/template-smile-gallery.php'));
+      if($page[0]->ID){
+        echo "<a href='".get_permalink($page[0]->ID)."' class='btn'>View Smile Gallery</a>";
+      }
+        
+      echo $args['after_widget'];
+    
+    }
+    
+  }
+		
+  public function form( $instance ) {
+    if (isset($instance[ 'title' ])){ $title = $instance[ 'title' ];}else{$title = __( 'Smile Gallery', 'widget_form_domain' );}
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Gallery Title:</label> 
+      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <p>
+      <label for="">Gallery Images Source:</label> 
+      <select class="widefat" id="gallery-source" name="gallery-source" type="text"> 
+	      <option>Select from Gallery Pages...</option>
+	      <?php $gallery_src = array( 'post_type' => 'page', 'orderby' => 'post_date','order' => 'DESC','posts_per_page' => -1, 'meta_query' => array( array( 'key' => '_wp_page_template', 'value' => 'template-gallery-grid.php' ) ) ); 
+		    $src = new WP_Query( $gallery_src );
+        while ( $src->have_posts() ) : $src->the_post(); 
+        global $post; $post_slug=$post->post_name;?>
+	      <option value="<?php echo $post_slug; ?>"><?php the_title(); ?></option>
+	      <?php endwhile; ?>
+      </select>
+    </p>
+    <?php
+  }
+	
+  public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+      return $instance;
+  }
+
+}
+}
+ //end check to see if this widget already exists 
 
 
 
