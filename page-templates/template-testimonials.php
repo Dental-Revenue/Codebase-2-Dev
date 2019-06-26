3c68aa83cadf761d75a427f1ce07d0a83ecb7298
zachatduckpin <zach@duckpindesign.com>
2019-06-26 14:37:47 -0400

    Add initial  reviews template
    Still need to add Live Reviews

---
+++ page-templates/template-testimonials.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Template Name: Testimonials 
+*/
+?>
+
+<?php get_header(); ?>
+	
+	<!-- page head -->
+  <?php get_template_part( 'partials/page-head' ); ?> 
+
+	<!-- main -->
+	<div role="main" class="testimonials-template">	
+		<div class="row">
+
+		  <div class="columns eight">
+				
+				<div class="main-content">
+				  <?php if (have_posts()) : while (have_posts()) : the_post();
+					  the_content();
+          endwhile; endif; ?>
+				</div>   				
+				
+				<div class="interior-testimonials"> 				
+  				<?php
+          $args = array( 'post_type' => 'testimonials', 'order' => 'ASC', 'orderby'=> 'menu_order', 'posts_per_page' => -1 );
+          $loop = new WP_Query( $args );
+          while ( $loop->have_posts() ) : $loop->the_post();
+    
+            $thumb_array = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
+            $thumb_url = $thumb_array['0'];
+            ?>
+    
+            <div class="testimonial">
+              <div class="testimonial-img">
+              <?php if(has_post_thumbnail()){
+                the_post_thumbnail( 'sm-square',array('alt' => get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true) ) );
+              }else{ ?>
+                <i class="icon ion-ios-chatboxes-outline"></i>
+              <?php } ?>
+              </div>
+              <div class="testimonial-text">
+                <h3><?php the_title(); ?> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/layout/g-logo.png" alt="Google Logo" class="g-logo" /></h3>
+                <p><?php echo get_the_content(); ?></p>
+                <?php if(get_post_meta( get_the_ID(), 'meta_testimonial_video', true )){ ?>
+                  <a href="http://www.youtube.com/watch?v=<?php echo get_post_meta( get_the_ID(), 'meta_testimonial_video', true ); ?>" class="popup-youtube">Watch Video <i class="icon ion-social-youtube-outline"></i></a>
+                <?php } ?>
+              </div>
+            </div>
+      
+          <?php endwhile; ?>
+  				
+				</div>
+				
+			</div> 
+			
+			<div class="columns four">
+				<?php get_sidebar(); ?>
+			</div>               
+
+		</div>
+	</div>
+	
+	<!-- horizontal rule -->
+	<hr class="row" />
+
+<?php get_footer(); ?>
