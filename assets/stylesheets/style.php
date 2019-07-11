0160aa84b87c6fd25879740ee165f29724a672b0
Derek Hargest <derek.h@rollwithduckpin.com>
2019-07-11 17:07:00 -0400

    Update style.php for SCSSPHP Update

--- assets/stylesheets/style.php
+++ assets/stylesheets/style.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once("../../../../../wp-load.php");
+$loadPath = preg_replace('/wp-content.*$/','',__DIR__);
+
+require_once($loadPath. "wp-load.php");
 $init_main_color = !empty(site_ops_brand_color(false)) ? site_ops_brand_color(false) : '#d93';
 $init_heading_font = !empty(site_ops_heading_font(false)) ? site_ops_heading_font(false) : 'Raleway';
 $init_body_font = !empty(site_ops_body_font(false)) ? site_ops_body_font(false) : 'Karla';
@@ -8,26 +10,26 @@ $init_button_style = !empty(site_ops_buttons_style(false)) ? site_ops_buttons_st
 $init_headline_case = !empty(site_ops_headline_case(false)) ? site_ops_headline_case(false) : 'uppercase';
 
 require_once  __DIR__ . '/../../inc/libs/scssphp/scss.inc.php';
-require_once  __DIR__ . '/../../inc/libs/scssphp/example/Server.php';
-use Leafo\ScssPhp\Compiler;
-use Leafo\ScssPhp\Server;
-$directory = __DIR__ ;
 
-$scss = new Compiler();
+use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Formatter\Compressed;
+
+$directory = __DIR__ ;
+$formatterName = "ScssPhp\\ScssPhp\\Formatter\\Compressed";
+
+$scss = new scssc();
 $scss->setVariables(array(
     'main-color' => $init_main_color,
     'heading-font' => $init_heading_font,
     'body-font' => $init_body_font,
     'buttons-style' => $init_button_style,
-    'headline-case' => $init_headline_case,
+    'headline-case' => $init_headline_case
 ));
-$scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
+$scss->setFormatter('scss_formatter_compressed');
 $scss->setImportPaths($directory);
-$scss->setLineNumberStyle(Compiler::LINE_COMMENTS);
 
-$server = new Server($directory, null, $scss);
-$server->serve();
-
-//echo $directory;
+$scssIn = file_get_contents($directory . '/style.scss');
+$cssOut = $scss->compile($scssIn);
+file_put_contents($directory . '/style.css', $cssOut);
 
 ?>
