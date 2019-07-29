9402845d31cda762e567ef376741750b711a1098
Codebase2016 <bburns@adsnextwebsites.com>
2019-07-29 16:15:30 -0400

    Update blog_columns.php

--- inc/meta/modules/blog_columns.php
+++ inc/meta/modules/blog_columns.php
@@ -1,12 +1,18 @@
 <?php	
 
 $box->add_field( array(
+	'name' => 'Headline',
+	'id' => $prefix.'headline',
+	'description' => 'Supports {subhead}',
+	'type' => 'text'
+));
+
+$box->add_field( array(
 	'name' => 'Post Category',
 	'id' => $prefix.'category',
 	'type' => 'select',
 	'options_cb' => 'cmb2_get_term_options',
 	'default' => 'all'
 ));
-
 
 ?>
