4fa3831514f605099c9f8e58048daff21aefc16d
Codebase2016 <bburns@adsnextwebsites.com>
2019-08-06 12:17:13 -0400

    Adds optional title, subtitle, and paragraph field

--- inc/meta/modules/reviews_google.php
+++ inc/meta/modules/reviews_google.php
@@ -3,7 +3,7 @@
 	$box->add_field( array(
 		'name' => 'Title',
 		'id' => $prefix.'title',
-		'type' => 'text',
+		'type' => 'textarea_small',
 		'default' => '(Patient Testimonials)',
 		'desc' => 'Supports (Title){Subtitle}[Paragraph]'
 	));
