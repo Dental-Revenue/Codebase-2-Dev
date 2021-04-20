8a57fc67ae694d13ec0663f29d0111e88232b2bd
Codebase2016 <jiyoon@dentalrevenue.com>
2021-04-20 10:52:39 -0400

    Replaced slider logos with static logos

--- inc/meta/modules/logo_scroll.php
+++ inc/meta/modules/logo_scroll.php
@@ -1,6 +1,6 @@
 <?php 
 	
-				$box->add_field( array(
+				/*$box->add_field( array(
 					'name'	=> '# of Slides to Show',
 					'id'		=> $prefix .'num_slides',
 			    'type' 	=> 'select',
@@ -9,22 +9,22 @@
 						'4' => 'Four Logos',
 						'5' => 'Five Logos',
 					),
-				));
+				));*/
 				$box->add_field( array(
 					'name' => 'Common Logos',
 					'id'   => $prefix .'common_images',
 					'type' => 'multicheck_inline',
 					'options' => array(
-						'/wp-content/themes/codebase-2/assets/images/layout/logo-ada.jpg' => 'ADA',
-						'/wp-content/themes/codebase-2/assets/images/layout/logo-aacd.jpg' => 'AACD',
-						'/wp-content/themes/codebase-2/assets/images/layout/logo-academy.jpg' => 'Academy of General Dentistry',
-						'/wp-content/themes/codebase-2/assets/images/layout/logo-cerec.jpg' => 'CEREC',
-						'/wp-content/themes/codebase-2/assets/images/layout/logo-invisalign.jpg' => 'Invisalign',
+						'/wp-content/themes/codebase-2/assets/images/layout/logo-ada.png' => 'ADA',
+						'/wp-content/themes/codebase-2/assets/images/layout/logo-aacd.png' => 'AACD',
+						'/wp-content/themes/codebase-2/assets/images/layout/logo-academy.png' => 'Academy of General Dentistry',
+						'/wp-content/themes/codebase-2/assets/images/layout/logo-cerec.png' => 'CEREC',
+						'/wp-content/themes/codebase-2/assets/images/layout/logo-invisalign.png' => 'Invisalign',
 					),
 				));
 				$box->add_field( array(
 					'name' => 'Custom Logos',
-					'description' => 'Logo should be placed on 400x168 background',
+					'description' => 'Logo should have no white space',
 					'id'   => $prefix .'images',
 					'type' => 'file_list',
 					'options' => array('url' => false),
@@ -32,6 +32,20 @@
 						'type' => array('image/gif','image/jpeg','image/png')
 					)
 				));	
+				$box->add_field( array(
+					'name' => 'Padding Top',
+					'id' => $prefix.'paddingtop',
+					'type' => 'text',
+					'default' => '30px',
+					'description' => 'Example: 30px'
+				));
+				$box->add_field( array(
+					'name' => 'Padding Bottom',
+					'id' => $prefix.'paddingbottom',
+					'type' => 'text',
+					'default' => '30px',
+					'description' => 'Example: 30px'
+				));
 
 
 ?>
