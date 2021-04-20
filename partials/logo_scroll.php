479a7b8e4719b15682dceba8c7d4160f6236d7c6
Codebase2016 <jiyoon@dentalrevenue.com>
2021-04-20 10:51:48 -0400

    Replaced slider logos with static logos

--- partials/logo_scroll.php
+++ partials/logo_scroll.php
@@ -1,20 +1,12 @@
-<?php $instance = $template_args['instance'];   
-	$num_slides = get_post_meta(get_the_id(),$instance.'_num_slides',true); ?>
+<?php
+$instance = $template_args['instance'];
+$images = get_post_meta( get_the_ID(), $instance.'_common_images', 1 );
+$entries = get_post_meta( get_the_ID(), $instance.'_images', 1 ); 
+$paddingtop = get_post_meta( get_the_ID(), $instance.'_paddingtop', 1 );
+$paddingbottom = get_post_meta( get_the_ID(), $instance.'_paddingbottom', 1 );
+?>
 
-  
-<div class="row">
-  <div class="slick-init slick-logo_scroll" data-slides="<?php echo $num_slides; ?>">
-    <?php $images = get_post_meta( get_the_ID(), $instance.'_common_images', 1 ); 
-	    foreach ( (array) $images as $image ) {
-	     ?>
-			<img src="<?php echo $image; ?>" alt="logo image" />  
-    <?php } ?>
-    <?php $entries = get_post_meta( get_the_ID(), $instance.'_images', 1 ); 
-	    if ($entries != '') {
-	    	foreach ( (array) $entries as $attachment_id => $attachment_url ) {
-		     $img = wp_get_attachment_image_src( $attachment_id, 'logo' );
-	     ?>
-			<img src="<?php echo $img[0]; ?>" alt="logo image" />  
-    <?php } } ?>
-  </div>
+<div class="row" style="padding-top:<?php echo $paddingtop; ?>;padding-bottom:<?php echo $paddingbottom; ?>">
+<?php
+if ($images != '') { foreach ( (array) $images as $image ) {?><img src="<?php echo $image; ?>" alt="logo image" /><?php } } ?><?php if ($entries != '') { foreach ( (array) $entries as $attachment_id => $attachment_url ) { $img = wp_get_attachment_image_src( $attachment_id, 'logo' );?><img src="<?php echo $img[0]; ?>" alt="logo image" /><?php } } ?>
 </div>
