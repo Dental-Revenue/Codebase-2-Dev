4b7bc0e14650345f91c12a729efde7f6541ae137
Codebase2016 <jiyoon@dentalrevenue.com>
2021-05-14 18:23:59 -0400

    Add files via upload

--- partials/specials_breaker.php
+++ partials/specials_breaker.php
@@ -12,9 +12,9 @@
 	$button_url = get_post_meta(get_the_id(),$instance.'_specials_breaker_button_url',true);	
 	$paragraph_mobile = get_post_meta(get_the_id(),$instance.'_paragraph_mobile',true);	
 ?>
-<?php if($paragraph_mobile === 'no' ){ ?><style>.specials_breaker h2:after {margin-bottom: 15px;}</style><?php } ?>
+<?php if($paragraph_mobile === 'no' && wp_is_mobile()) { ?><style>.specials_breaker h2:after {margin-bottom: 15px;}</style><?php } ?>
 <div class="row">
   <?php if(!empty($headline)){ ?><h2 class="<?php echo $headline_style; ?>"><?php echo $headline; ?></h2><?php } ?>
-		<?php if(!empty($specials_text) && $paragraph_mobile === 'yes' ){ ?><p><?php echo $specials_text; ?></p><?php } ?>
+  <?php if(!empty($specials_text) && !wp_is_mobile() ){ ?><p><?php echo $specials_text; ?></p><?php } ?><?php if( wp_is_mobile() && $paragraph_mobile === 'yes' && !empty($specials_text) ){ ?><p><?php echo $specials_text; ?></p><?php } ?>
   <?php if ($button_url != ''){?><a href="<?php echo $button_url; ?>" class="btn"><?php echo $button_text; ?></a><?php } ?>
 </div>
--- partials/static_intro_section.php
+++ partials/static_intro_section.php
@@ -3,10 +3,11 @@
 	$appearance_info = get_option( 'appearance_info');
 	$headline_style = $appearance_info['headline_style'];
 
-	$paragraph = apply_filters('the_content', get_post_meta(get_the_id(),$instance.'_paragraph',true));
+	$paragraph = get_post_meta(get_the_id(),$instance.'_paragraph',true);
+	//$paragraph = apply_filters('the_content', get_post_meta(get_the_id(),$instance.'_paragraph',true));
 	$paragraph_mobile = get_post_meta(get_the_id(),$instance.'_paragraph_mobile',true);	
 ?>
-<?php if($paragraph_mobile === 'no' ){ ?><style>.static_intro_section h2:after {margin-bottom: 15px;}</style><?php } ?>
+<?php if($paragraph_mobile === 'no' && wp_is_mobile()) { ?><style>.static_intro_section h2:after {margin-bottom: 15px;}</style><?php } ?>
 <div class="row sm">
   
   <?php
@@ -16,8 +17,7 @@
 	?>
   
   <h2 class="<?php echo $headline_style; ?>"><?php echo $headline; ?></h2>
-  <div class="rte">
-	  <?php if(!empty($paragraph) && $paragraph_mobile === 'yes' ){ ?><?php echo $paragraph; ?><?php } ?>
+  <div class="rte"><?php if(!empty($paragraph) && !wp_is_mobile() ){ ?><p><?php echo $paragraph; ?></p><?php } ?><?php if( wp_is_mobile() && $paragraph_mobile === 'yes' && !empty($paragraph) ){ ?><p><?php echo $paragraph; ?></p><?php } ?>
     <?php if ($link != '') { ?>
 			<a href="<?php echo $link; ?>" class="btn solid">Learn More</a>
 		<?php } ?>
