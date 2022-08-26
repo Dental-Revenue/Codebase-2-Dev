60bed13f7cc756d5c1c8945b2d0bf4450d2b763a
PaulDRev <paul@dentalrevenue.com>
2022-08-26 12:13:00 -0400

    Undo last commit

--- partials/reviews_testimonials_fold.php
+++ partials/reviews_testimonials_fold.php
@@ -47,7 +47,7 @@
 			<?php endif; ?>
         	<p><?php echo wp_trim_words( get_the_content(), 120, "..." ); ?></p>
         	<?php if($services && $services!=''){ ?>
-        		<p class="rtf-heading">What we did</p>
+        		<h4 >What we did</h4>
 						<p><?php echo $services; ?></p>
 					<?php } ?>
       	</div>
