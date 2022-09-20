2b51cf07b96b485c3e700a52f47ee7f415f3b196
PaulDRev <paul@dentalrevenue.com>
2022-09-20 10:19:40 -0400

    Add Terms Of Use template and remove code from functions

---
+++ page-templates/template-terms-of-use.php
@@ -0,0 +1,40 @@
+<?php
+/*
+    Template Name: Terms Of Use
+*/
+get_header();
+get_template_part('partials/page-head');
+?>
+<!-- main -->
+<div role="main">
+    <div class="row">
+        <div class="columns twelve">
+            <div class="main-content">
+                <div class="terms-of-use-content">
+                    <h3><?php site_ops_practice_name(); ?> Website Terms of Use – User Agreement</h3>
+                    <p>By using this website, you are acknowledging and agreeing to the terms of use and conditions outlined in within this policy. Please read carefully.</p>
+                    <p>We reserve the right to amend this information at any time, for any reason. We will post a notice that our Website Terms of Use has been amended by revising the "Last updated" date at the bottom of this page.</p>
+                    <p>Please refer to our Privacy Statement for concerns about how we collect, use and maintain your information.</p>
+                    <h3>Ownership</h3>
+                    <p>This website, its entire contents including text, photos and materials available for download are the property of <?php site_ops_practice_name(); ?> and/or its affiliates. This site offers educational information on the organization, products/services and resources. This information is not intended to be a substitute for professional or medical advice, diagnosis or treatment. Always seek the advice of your doctor if you have questions regarding a medical condition.</p>
+                    <h3>Acceptable Use</h3>
+                    <p>You are permitted to access the site for your personal, non-commercial use. Unlawful usage of the site or violations to these terms and conditions are prohibited and may result in legal action.</p>
+                    <h3>Copyright</h3>
+                    <p>The entire contents of <?php site_ops_practice_name(); ?>’s website including: logos, trademarks, service marks, brand names, information, materials, interfaces, computer code, databases, text, images, photographs, audio and visual material, etc as well as design, structure and assembly, are the property of [practice_name], its licensors, partners, sponsors, advertisers, content providers or other third parties and is protected by law. The reproduction, transmission, distribution, sale, publication or otherwise is prohibited without prior written consent obtained from <?php site_ops_practice_name(); ?> or the owner of the content if <?php site_ops_practice_name(); ?> is not the owner.You may not alter, delete or conceal any copyright or other notices appearing in the <?php site_ops_practice_name(); ?> content including notices appearing on materials available for download.</p>
+                    <h3>Disclaimers</h3>
+                    <p>While we make every attempt to ensure that the information provided on this website is timely and accurate, we can make warranties about the accuracy or completeness. We assume no liability for interruptions, errors, computer viruses or other hazards resulting from your use of this site.</p>
+                    <h3>No Medical Advice</h3>
+                    <p>This website is intended to provide educational information about the organization and the products/services it provides. This is intended to be general in nature and should not be mistaken for professional medical advice. Before making decisions about your health care, please consult with your personal physician.</p>
+                    <h3>Limitation of Liability</h3>
+                    <p>Our website may link to other websites, as other websites may link to ours. We do not review, control or take responsibility for the accuracy of content on these websites. Nor do we review, control or take responsibility for their privacy and security policies. Accessing these websites is at your own risk and <?php site_ops_practice_name(); ?> shall assume no liability for users use on any linked sites.</p>
+                    <h3>Privacy</h3>
+                    <p>Most information transmitted electronically is not secure, therefore, we can make no guarantees to privacy or confidentiality. <?php site_ops_practice_name(); ?> and its affiliates take extensive measures to protect sensitive information. Personal medical information should never be sent through our website. Please review our Privacy Statement for further information.</p>
+                    <h3>Contact Us</h3>
+                    <p>Please contact us with any questions regarding our Website Terms of Use by contacting <?php site_ops_practice_name(); ?> at <?php site_ops_current_patient_phone(); ?></p>
+                    <p><?php site_ops_practice_name(); ?>, All Rights Reserved</p>
+                    <p>Last Update: 02/03/2016</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
